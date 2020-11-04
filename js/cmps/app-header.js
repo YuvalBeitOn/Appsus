92b957188c9338a3145de87ff18cd5786ccdc193
Guy Indepurker <guy235@gmail.com>
2020-11-04 15:05:02 +0200

    add color to header and footer

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -1,11 +1,11 @@
 export default {
     name: 'app-header',
     template: `
-    <header class="app-header">
+    <header class="app-header flex space-between  align-center">
         <div class="logo-container">
              <h1 class="logo">APPSUS</h1>
         </div>
-        <nav class="main-nav flex">
+        <nav class="main-nav flex space-between">
             <router-link to="/">Home</router-link>
             <router-link to="/email">Email</router-link>
             <router-link to="/keep">Keep</router-link>

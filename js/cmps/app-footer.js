69b4996973f23d41fecdd651e0121359437156c2
Guy Indepurker <guy235@gmail.com>
2020-11-04 13:20:32 +0200

    change css

---
+++ js/cmps/app-footer.js
@@ -0,0 +1,14 @@
+
+
+export default {
+  name:'footer',
+  template: `
+    <section class="app-footer">
+   <div class="container flex column align-center">
+       <small class="copy-rights mr-5"> Copyrights &copy; 2020 Guy Indepurker , Orel Vaizman ,Yuval Beiton. All rights reserved</small>
+   </div>
+</section>
+    `,
+  
+}
+

0c2a2525954e7590b7f2eb6540b64f9cf7e14edf
Orel Vaizman <orelvaizman@h-MacBook-Pro-sl-Orel.local>
2020-11-04 19:27:55 +0200

    updateOrel

--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -1,14 +1,36 @@
-
+import { emailService } from '../../email/service/email-service.js';
 export default {
     props: ['mail'],
     template: `
-        <section class="mail-preview">
-           <h4>{{mail.sender}}</h4>
-           <h4>{{mail.subject}}</h4>
-           <h4>{{mail.body}}</h4>
-           <h4>{{mail.sentAt}}</h4>
+        <section class="email-preview flex">
+            <section v-if="isSelected" class="email-buttons">
+            <button @click="onRemoveMail()"><i class="fas fa-trash"></i>
+            </button>
+            </section>
+
+            <input type="checkbox" v-model="isSelected"/>
+            <label for="star-checkbox">{{starMarker}}</label>
+            <input v-show="false" id="star-checkbox" type="checkbox" v-model="isStarred"/>
+           <div class="email-sender">{{mail.sender}}</div>
+           <div class="email-subject">{{mail.subject}}</div>
+           <div class="email-body">{{mail.body}}</div>
+           <div class="email-date">{{mail.sentAt}}</div>
         </section>
     `,
-    created(){
+    data() {
+        return {
+            isSelected: false,
+            isStarred: false
+        }
+    },
+    methods: {
+        onRemoveMail() {
+            emailService.removeMail(this.mail.id)
+        }
+    },
+    computed: {
+        starMarker() {
+           return (this.isStarred) ? 'STARREDDDDDDD' : 'NOT'
+        }
     }
 }

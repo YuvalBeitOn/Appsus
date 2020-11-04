4211b19a3aeb920b762d8d116a7813125d77645b
Guy Indepurker <guy235@gmail.com>
2020-11-04 15:39:28 +0200

    add service

--- js/apps/email/service/email-service.js
+++ js/apps/email/service/email-service.js
@@ -1,5 +1,30 @@
 import { utilService } from '../../../services/util.service'
-
+var gMails = _createMails()
 export const emailService = {
-// funcs,
+    getMails,
 };  
+
+function getMails(){
+    return Promise.resolve(gMails)
+}
+
+function _createMails(){
+    gMails = []
+    gMails.push(_createMail('Guy','New day',utilService.makeLorem(15)))
+    gMails.push(_createMail('Moshe','New day',utilService.makeLorem(15)))
+    gMails.push(_createMail('Manham','New day',utilService.makeLorem(20)))
+    gMails.push(_createMail('Bob Habani','I Have Work',utilService.makeLorem(5)))
+    gMails.push(_createMail('Gantz','Wassap? ',utilService.makeLorem(10)))
+    return gMails;
+}
+
+function _createMail(name,subject,body,isRead=false){
+return {
+    id:utilService.makeId(),
+    name,
+    subject,
+    body,
+    isRead,
+    sentAt:Date.now()
+}
+}
--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,7 +1,8 @@
 export const utilService = {
     storeToStorage,
     loadFromStorage,
-    makeId
+    makeId,
+    makeLorem
 }
 
 function storeToStorage(key, value) {
@@ -18,6 +19,15 @@ function makeId(length = 5) {
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (var i = 0; i < length; i++) {
         txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
+}
+function makeLorem(size = 10) {
+    var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn'];
+    var txt = '';
+    while (size > 0) {
+        size--;
+        txt += words[Math.floor(Math.random() * words.length)] + ' ';
     }
     return txt;
 }

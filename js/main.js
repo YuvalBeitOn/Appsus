5312f4aae9f6a772651103fce95c1efdbfad465e
Yuval Beit On <yuvalbeiton@gmail.com>
2020-11-04 14:00:22 +0200

    haval

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { myRouter } from './routes.js'
+// import { myRouter } from './routes.js'
 import appHeader from "./cmps/app-header.js";
 import appFooter from "./cmps/app-footer.js";
 import userMsg from "./cmps/user-msg.js"

ee9edd27ca5555216d574eb9acf8ef9604a682dc
Guy Indepurker <guy235@gmail.com>
2020-11-05 00:35:16 +0200

    fix long text

--- js/cmps/long-text.cmp.js
+++ js/cmps/long-text.cmp.js
@@ -14,7 +14,7 @@ export default {
       textToShow() {
         const txt = this.txt;
         if (txt.length > 100 && !this.isShowAll) {
-          return txt.substring(0, 100) + "...";
+          return txt.substring(0, 85) + "...";
         } else return txt;
       },
      

776072307af70e978cfca0d5622a85fef8bf8b5f
Orel Vaizman <orelvaizman@h-MacBook-Pro-sl-Orel.local>
2020-11-04 15:40:12 +0200

    Start

---
+++ js/apps/email/cmps/book-list.cmp.js
@@ -0,0 +1,12 @@
+export default {
+    props:[emails],
+    template:
+     `<section>
+        <h1>I am your email List component</h1>
+    </section>`,
+    data(){
+        return{
+
+        }
+    }
+}
--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -1,7 +1,21 @@
+import { emailService } from '../../email/service/email-service.js'
 export default {
     template: `
         <section class="email-app">
         <h1>I am your Email APP</h1>
+
     </section>
-    `
+    `,
+    data() {
+        return {
+            mails: null
+        }
+    },
+    created() {
+        emailService.getEmails()
+            .then(mails => this.mails = mails)
+    },
+    components: {
+        emailService
+    }
 }

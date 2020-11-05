f1014fabc58f9e67ed99ff2ed1f0dbae679554a1
Orel Vaizman <orelvaizman@h-MacBook-Pro-sl-Orel.local>
2020-11-05 16:15:39 +0200

    HalfWayParmas

--- js/routes.js
+++ js/routes.js
@@ -2,12 +2,6 @@ import homePage from './pages/home-page.cmp.js'
 import emailApp from './apps/email/pages/email-app.cmp.js'
 import keepApp from './apps/keep/pages/keep-app.cmp.js'
 import emailDetails from './apps/email/pages/email-details.cmp.js'
-// TODO: MOVE THIS LINKS TO APP MAIL!!!! ?
-import emailAll from './apps/email/pages/email-all.cmp.js'
-import emailStar from './apps/email/pages/email-star.cmp.js'
-import emailSent from './apps/email/pages/email-sent.cmp.js'
-import emailRemove from './apps/email/pages/email-remove.cmp.js'
-import emailDrafts from './apps/email/pages/email-drafts.cmp.js'
 import emailCompose from './apps/email/cmps/email-compose.cmp.js'
 const myRoutes = [{
     path: '/',
@@ -22,33 +16,10 @@ const myRoutes = [{
     component: emailDetails
 },
 //TODO: MOVE THIS PATHS TO MAIL APP ROUTS 
-// {
-//     path: '/email/all',
-//     component: emailAll
-// },
-// {
-//     path: '/email/stared',
-//     component: emailStar
-// },
-// {
-//     path: '/email/stared',
-//     component: emailStar
-// },
-// {
-//     path: '/email/sent',
-//     component: emailSent
-// },
-// {
-//     path: '/email/drafts',
-//     component: emailDrafts
-// },
-// {
-//     path: '/email/remove',
-//     component: emailRemove
-// }, {
-//     path: '/email/compose',
-//     component: emailCompose
-// },
+{
+    path: '/email/compose',
+    component: emailCompose
+},
 // /////////////////////////////////////////////
 {
     path: '/keep',

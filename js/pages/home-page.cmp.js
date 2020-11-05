d7baf7aa6563b20a012e99c7f375c57783a9e9ed
Guy Indepurker <guy235@gmail.com>
2020-11-05 02:23:09 +0200

    add jumbatron

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,8 +1,22 @@
 export default {
     template: `
-    <section>
-        <h1>Home Sweet Home</h1>
-    </section>
+    <section class="home-page ">
+    <div class="jumbotron flex column  wrap align-center">
+        <h1 class="title-home-page">Home Sweet Home</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+
+It uses utility classes for typography and spacing to space content out within the larger container.</p>
+
+<div class="controls-btn">
+            <router-link to="/email/inbox" exact><button class="btn-home">Email</button></router-link>
+            <router-link to="/keep" exact><button class="btn-home">Keep</button>
+</router-link>
+            <router-link to="/books" exact><button class="btn-home">Books</button>
+</router-link>
+
+</div>
+</div>    
+</section>
     `,
 
 }

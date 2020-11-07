be3d73eb064d40c104c8efafc760c63ba44ddd97
Guy Indepurker <guy235@gmail.com>
2020-11-07 16:21:26 +0200

    add books

--- js/routes.js
+++ js/routes.js
@@ -3,6 +3,9 @@ import emailApp from './apps/email/pages/email-app.cmp.js'
 import keepApp from './apps/keep/pages/keep-app.cmp.js'
 import emailDetails from './apps/email/pages/email-details.cmp.js'
 import emailCompose from './apps/email/cmps/email-compose.cmp.js'
+import bookApp from './apps/books/pages/book-app.cmp.js'
+import bookDetails from './apps/books/pages/book-details.cmp.js'
+// import bookAdd from './apps/books/pages/add-book.cmp.js'
 const myRoutes = [{
     path: '/',
     component: homePage
@@ -11,10 +14,20 @@ const myRoutes = [{
     path: '/email/compose/',
     component: emailCompose
 },
+{
+    path: '/book',
+    component: bookApp,
+},
 // {
-//     path: '/email/compose/:mailId',
-//     component: emailCompose
+//     path: '/book/add',
+//     component: bookAdd,
 // },
+
+{
+    path: '/book/:bookId',
+    component: bookDetails
+},
+
 {
     path: '/email/:mailsCategory',
     component: emailApp

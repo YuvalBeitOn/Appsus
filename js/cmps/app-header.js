8e3fad25ff3dc7e38f390405e34b66c03dd49db4
Guy Indepurker <guy235@gmail.com>
2020-11-07 12:48:40 +0200

    add home page

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -1,27 +1,20 @@
+
 export default {
     name: 'app-header',
     template: `
-    <header class="app-header flex space-between align-center">
-        <div class="logo-container">
-             <h1 class="logo">Appsus</h1>
+    <header class="app-header ">
+    <div class="container flex space-between align-center">
+        <div class="logo-container flex">
+        <img src="../../assets/imgs/logo.svg" class="logo-svg " alt="">
+             <h1 class="logo ml-5">Appsus</h1>
         </div>
         <nav class="main-nav flex space-between">
             <router-link to="/" exact><i class="nav-icon-header fas fa-home"></i></router-link>
-            <!-- TODO: REMOVE THIS LINE BELOW FROM NOTE! :-->
-            <router-link to="/email/inbox" exact><i class="nav-icon-header fas fa-envelope"></i><span class="ml-5">Email</span></router-link>
-           <!-- ////////////////////////////////////////////////////////// -->
-            <!-- TODO: MOVE THIS LINKS TO MAIL APP! -->
-            <!-- <router-link to="/email/inbox" ><i class="nav-icon-header fas fa-inbox"></i><span class="ml-5">Inbox</span></router-link>
-            <router-link to="/email/all" ><i class="nav-icon-header fas fa-envelope"></i><span class="ml-5">All Mails</span></router-link>
-            <router-link to="/email/stared" exact><i class="nav-icon-header fas fa-star"></i> <span class="ml-5">Starred</span></router-link>
-            <router-link to="/email/sent" exact><i class="nav-icon-header fas fa-share"></i> <span class="ml-5">Sent</span></router-link>
-            <router-link to="/email/trash" exact><i class="nav-icon-header fas fa-trash"></i> <span class="ml-5">Deleted</span></router-link>
-            <router-link to="/email/drafts" exact><i class="nav-icon-header fas fa-file"></i> <span class="ml-5">Drafts</span></router-link>
-            <router-link to="/email/compose" exact><i class="nav-icon-header fas fa-file"></i> <span class="ml-5">Compose</span></router-link> -->
-            <!-- ///////////////////////////////////////////////// -->
-            <router-link to="/keep" exact><i class="nav-icon-header fas fa-sticky-note"></i></router-link>
-            <router-link to="/books" exact><i class="nav-icon-header fas fa-book"></i></router-link>
+            <router-link to="/email/inbox" exact><i class="nav-icon-header word-red fas fa-envelope"></i></router-link>
+            <router-link to="/keep" exact><i class="nav-icon-header word-yellow fas fa-sticky-note"></i></router-link>
+            <router-link to="/books" exact><i class="nav-icon-header word-green fas fa-book"></i></router-link>
         </nav>
+        </div>
     </header>
       `
 }
--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,7 +1,8 @@
 export default {
-    template: `
+  template: `
     <section class="home-page ">
-    <div class="jumbotron flex column  wrap align-center">
+    <div class="jumbotron ">
+        <div class="container flex column  wrap align-center">
         <h1 class="title-home-page">Home Sweet Home</h1>
         <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
 
@@ -15,15 +16,116 @@ It uses utility classes for typography and spacing to space content out within t
 </router-link>
 
 </div>
+</div>
 </div>   
-<div class="why-us">
-<h2 class="why-us">Our Features</h2>
-<div class="box-features">
-<i class="icon-box fas fa-envelope"></i>
-<h4 class="title-box"></h4>
+<!--Section Our features -->
+<section class="our-features">
+<div class="container features-container">
+<h2 class="why-us-title">Our Features</h2>
+<div class="why-us flex  align-center justify-center">
+<div class="box-features flex column wrap justify-center align-center">
+<i class="icon-box word-red fas fa-envelope"></i>
+<h3 class="title-box">Mail-Box</h3>
+<p class="text-box">Send Mail is much more easier! now you can send mail fast you can drem!</p>
+<router-link to="/email/inbox" exact><button class="btn-home">Learn More</button>
+</router-link>
+</div>
+<div class="box-features flex column wrap justify-center align-center">
+<i class="icon-box word-yellow fas fa-sticky-note"></i>
+<h3 class="title-box ">Keeps</h3>
+<p class="text-box">Save your note and your ides in one app! now you can save all the things you need to do or impotent things and just write its powerful tool!</p>
+<router-link to="/keep" exact><button class="btn-home">Learn More</button>
+</router-link>
+</div>
+<div class="box-features flex column wrap justify-center align-center">
+<i class="icon-box word-green fas fa-book"></i>
+<h3 class="title-box">Books</h3>
+<p class="text-box">Save your favorite books!now you can save all the books that you love and looking for new books!</p>
+<router-link to="/book" exact><button class="btn-home">Learn More</button>
+</router-link>
 </div>
 </div> 
+</div>
+</section>
+<!-- Section info app -->
+<section class="info-app">
+<div class="box-info keep-info">
+<div class="container flex wrap">
+<div class="info-content flex column justify-center align-center">
+    <h2 class="title-box title-info">Save your thoughts, wherever you are</h2>
+    <p class="text-box text-info">
+Quickly filter and search for notes by color and other attributes like lists with images, audio notes with reminders or just see shared notes. Find what you're looking for even faster, and let Keep do the remembering for you.
+</p>
+<router-link to="/keep" exact><button class="btn-home">Learn More</button>
+</router-link>
+</div>
+<img src="../../assets/imgs/mocupkeep.jpg" class="image-info"  alt="">
+</div>
+</div>
+
+<div class="box-info mail-info">
+<div class="container flex wrap">
+<img src="../../assets/imgs/mocupkeep.jpg" class="image-info"  alt="">
+<div class="info-content flex column justify-center align-center">
+    <h2 class="title-box title-info">Send your mail, wherever you are</h2>
+    <p class="text-box text-info">
+Quickly filter and search for notes by color and other attributes like lists with images, audio notes with reminders or just see shared notes. Find what you're looking for even faster, and let Keep do the remembering for you.
+</p>
+<router-link to="/email/inbox" exact><button class="btn-home">Learn More</button>
+</router-link>
+</div>
+</div>
+</div>
+
+<div class="box-info book-info">
+<div class="container flex wrap">
+<div class="info-content flex column justify-center align-center">
+    <h2 class="title-box title-info">Save your Books, wherever you are</h2>
+    <p class="text-box text-info">
+Quickly filter and search for notes by color and other attributes like lists with images, audio notes with reminders or just see shared notes. Find what you're looking for even faster, and let Keep do the remembering for you.
+</p>
+<router-link to="/book" exact><button class="btn-home">Learn More</button>
+</router-link>
+</div>
+<img src="../../assets/imgs/mocupkeep.jpg" class="image-info"  alt="">
+
+</div>
+</div>
+</section>
+<h2 class="flex justify-center">Our Team</h2>
+<section class="our-team container flex row justify-center align-center">
+    <div class="box-team flex column justify-center align-center wrap">
+<!-- <img src="../../assets/imgs/team/team-guy.png" class="image-team"  alt=""> -->
+    <h2 class="title-box title-team">Name Name</h2>
+    <p class="text-box text-team">Lorem Ipsum Dolar Smit</p>
+    <ul class="social-icons clean-list flex justify-center">
+<li class="team-icon-social"><a href="https://www.facebook.com/Guy.Indepurker"><i class="item-social fab fa-facebook-f"></i></a></li>
+<li class="team-icon-social"><a href="https://www.linkedin.com/in/guy-indepurker-5778091a4/"><i class="item-social fab fa-linkedin-in"></i></a></li>
+<li class="team-icon-social"><a href="https://github.com/guyindepurker"><i class="item-social fab fa-github"></i></a></li>
+</ul>
+    </div>
+    <div class="box-team flex column justify-center align-center wrap">
+<!-- <img src="../../assets/imgs/team/team-guy.png" class="image-team"  alt=""> -->
+    <h2 class="title-box title-team">Name Name</h2>
+    <p class="text-box text-team">Lorem Ipsum Dolar Smit</p>
+    <ul class="social-icons clean-list flex justify-center">
+<li class="team-icon-social"><a href="https://www.facebook.com/Guy.Indepurker"><i class="item-social fab fa-facebook-f"></i></a></li>
+<li class="team-icon-social"><a href="https://www.linkedin.com/in/guy-indepurker-5778091a4/"><i class="item-social fab fa-linkedin-in"></i></a></li>
+<li class="team-icon-social"><a href="https://github.com/guyindepurker"><i class="item-social fab fa-github"></i></a></li>
+</ul>
+    </div>
+    <div class="box-team flex column justify-center align-center wrap">
+<!-- <img src="../../assets/imgs/team/team-guy.png" class="image-team"  alt=""> -->
+    <h2 class="title-box title-team">Name Name</h2>
+    <p class="text-box text-team">Lorem Ipsum Dolar Smit</p>
+    <ul class="social-icons clean-list flex justify-center">
+<li class="team-icon-social"><a href="https://www.facebook.com/Guy.Indepurker"><i class="item-social fab fa-facebook-f"></i></a></li>
+<li class="team-icon-social"><a href="https://www.linkedin.com/in/guy-indepurker-5778091a4/"><i class="item-social fab fa-linkedin-in"></i></a></li>
+<li class="team-icon-social"><a href="https://github.com/guyindepurker"><i class="item-social fab fa-github"></i></a></li>
+</ul>
+    </div>
+</section>
+
 </section>
     `,
-
-}
+};

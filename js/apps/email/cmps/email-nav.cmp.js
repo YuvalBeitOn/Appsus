42afcc942c6e909cd1e39a8569e3f7a9a9c31012
Guy Indepurker <guy235@gmail.com>
2020-11-07 17:23:07 +0200

    fix compose pic

--- js/apps/email/cmps/email-nav.cmp.js
+++ js/apps/email/cmps/email-nav.cmp.js
@@ -4,7 +4,7 @@ export default {
     template: `
     <section class="nav-bar-email">
     <div class="container-controls-nav flex column wrap">
-    <div @click="openCompose" class="compose-container mb-5 flex column  align-center"><span class="title-compose">Compose</span><img class="compos-icon ml-5" src="../../../../assets/imgs/compose.png"></div>
+    <div @click="openCompose" class="compose-container mb-5 flex column  align-center"><span class="title-compose">Compose</span><img class="compos-icon ml-5" src="assets/imgs/compose.png"></div>
             <router-link class="link-nav" to="/email/inbox" exact><i class="nav-icon-header fas fa-inbox"></i><span class="ml-5">Inbox</span></router-link>
             <router-link class="link-nav" to="/email/all" exact><i class="nav-icon-header fas fa-envelope"></i><span class="ml-5">All Mails</span></router-link>
             <router-link class="link-nav" to="/email/starred" exact><i class="nav-icon-header fas fa-star"></i> <span class="ml-5">Starred</span></router-link>
